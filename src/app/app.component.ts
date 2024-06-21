d9070ab3a28efab8c52b815425e46721af81013b
Alan <alan-hd@hotmail.com>
2024-06-20 21:42:26 -0300

    Criando linhas e colunas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrincipalComponent } from "./principal/principal.component";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, PrincipalComponent]
+    imports: [RouterOutlet, PrincipalComponent, FormsModule, CommonModule]
 })
 export class AppComponent {
   title = 'api-front';
--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Cliente } from '../modelo/Cliente';
+import { ClienteService } from '../servico/cliente.service';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './principal.component.html',
-  styleUrl: './principal.component.css'
+  styleUrl: './principal.component.css',
 })
 export class PrincipalComponent {
+  // Variavel para visibilidade dos botões
 
+  btnCadastro: boolean = true;
+
+  //  JSON de clientes
+
+  clientes: Cliente[] = [];
+
+  // Construtor
+
+  constructor(private servico: ClienteService) {}
+
+  // Método para selecionar clientes
+
+  selecionar(): void {
+    this.servico.selecionar().subscribe((retorno) => (this.clientes = retorno));
+  }
+
+  // Método de inicialização
+
+  ngOnInit() {
+    this.selecionar();
+  }
 }
