3dc2aa9880a2c674e66b37b92a789d7ea1119067
Alan <alan-hd@hotmail.com>
2024-06-21 15:26:54 -0300

    adicionando metodos

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -2,18 +2,25 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Cliente } from '../modelo/Cliente';
 import { ClienteService } from '../servico/cliente.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css',
 })
 export class PrincipalComponent {
+  // Objeto cliente
+  cliente = new Cliente();
+
   // Variavel para visibilidade dos botões
 
   btnCadastro: boolean = true;
+
+  // variavel para visibilidade dos botoes
+  tabela: boolean = true;
 
   //  JSON de clientes
 
@@ -27,6 +34,37 @@ export class PrincipalComponent {
 
   selecionar(): void {
     this.servico.selecionar().subscribe((retorno) => (this.clientes = retorno));
+  }
+
+  // Método cadastrar
+
+  cadastrar(): void {
+    this.servico.cadastrar(this.cliente).subscribe((retorno) => {
+      // Cadastrar o cliente no vetor
+      this.clientes.push(retorno);
+
+      // Limpar formulario
+
+      this.cliente = new Cliente();
+
+      // Mensagem
+
+      alert('Cliente cadastrado com sucesso');
+    });
+  }
+
+  // metodo para selecionar um cliente especifico
+
+  selecionarCliente(posicao: number): void {
+    // Selecionar cliente no vetor
+
+    this.cliente = this.clientes[posicao];
+
+    // Visibilidade botoes
+    this.btnCadastro = false;
+
+    // Visibilidade tabela
+    this.tabela = false;
   }
 
   // Método de inicialização
--- src/app/servico/cliente.service.ts
+++ src/app/servico/cliente.service.ts
@@ -19,4 +19,17 @@ export class ClienteService {
   selecionar():Observable<Cliente[]>{
     return this.http.get<Cliente []>(this.url);
   }
+
+  // Método para cadastrar cliente
+  cadastrar(obj:Cliente): Observable<Cliente>{
+    return this.http.post<Cliente>(this.url, obj);
+  }
+
+  
+  // Método para editar cliente
+  editar(obj:Cliente): Observable<Cliente>{
+    return this.http.put<Cliente>(this.url, obj);
+  }
+
+
 }
